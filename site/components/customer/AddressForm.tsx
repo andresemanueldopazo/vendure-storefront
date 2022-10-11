da3175b01c7f25f8e159ca60c48369895dd00314
andresemanueldopazo <andresemanueld@gmail.com>
2022-10-11 02:31:17 -0300

    fix(site): set customer address
    The layout hid part of the form.

--- site/components/customer/AddressForm.tsx
+++ site/components/customer/AddressForm.tsx
@@ -44,24 +44,23 @@ const TextInput = ({ label, ...props }: any) => {
         </Popover.Trigger>
         {!!errorsExceptRequired?.length && (
           <Popover.Portal>
-            <div>
-              <Popover.Content
-                sideOffset={5}
-                onOpenAutoFocus={(event) => event.preventDefault()}
-              >
-                {errorsExceptRequired.map((error, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="border border-secondary bg-primary text-center"
-                    >
-                      {error}
-                    </div>
-                  )
-                })}
-                <Popover.Arrow/>
-              </Popover.Content>
-            </div>
+            <Popover.Content
+              sideOffset={5}
+              onOpenAutoFocus={(event) => event.preventDefault()}
+              className="z-40"
+            >
+              {errorsExceptRequired.map((error, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="border border-secondary bg-primary text-center"
+                  >
+                    {error}
+                  </div>
+                )
+              })}
+              <Popover.Arrow/>
+            </Popover.Content>
           </Popover.Portal>
         )}
       </Popover.Root>
@@ -86,151 +85,149 @@ const AddressForm = () => {
         </Button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <div>
-          <Dialog.Overlay
-            className={cn(
-              "fixed inset-0 grid justify-items-center items-center",
-              theme === "light" ? "bg-black/30" : "bg-white/30",
-            )}
+        <Dialog.Overlay
+          className={cn(
+            "z-40 fixed inset-0 grid justify-items-center items-center",
+            theme === "light" ? "bg-black/30" : "bg-white/30",
+          )}
+        >
+          <Dialog.Content
+            className="fixed w-3/4 sm:w-3/5 md:w-2/5 overflow-y-auto max-h-screen p-2 bg-primary"
           >
-            <Dialog.Content
-              className="fixed w-3/4 sm:w-3/5 md:w-2/5 overflow-y-auto max-h-screen p-2 bg-primary"
+            <Formik
+              initialValues={{
+                company: '',
+                streetLine: '',
+                phoneNumber: '',
+                province: '',
+                postalCode: '',
+                city: '',
+                country: '',
+              }}
+              validate={async (values) => {
+                const schema = Yup.object({
+                  streetLine: Yup.string()
+                    .required('Required'),
+                  phoneNumber: Yup.number()
+                    .positive('Must be a positive number')
+                    .required('Required'),
+                  city: Yup.string()
+                    .required('Required'),
+                  province: Yup.string()
+                    .required('Required'),
+                  postalCode: Yup.string()
+                    .required('Required'),
+                  country: Yup.string()
+                    .required('Required'),
+                })
+
+                return await schema.validate(values, { abortEarly: false })
+                .then(() => {})
+                .catch((err: Yup.ValidationError) => {
+                  const errors = err.inner
+                  return errors.reduce<{[key: string]: string[]}>((a, error) => {
+                    const field = error.path
+                    const params = error.params
+  
+                    if (!params || !field) {
+                      return a
+                    }
+  
+                    const errorMessages = error.type !== 'typeError'? (
+                      error.errors
+                    ) : params.originalValue !== "" ? (
+                      [`Must be a ${params.type}`]
+                    ) : (
+                      ["Required"]
+                    )
+  
+                    return field in a ? (
+                      {...a, [field]: [...a[field], ...errorMessages]}
+                    ) : (
+                      {...a, [field]: errorMessages}
+                    )
+                  }, {})
+                })
+              }}
+              onSubmit={async (
+                {
+                  phoneNumber,
+                  company,
+                  streetLine,
+                  postalCode,
+                  province,
+                  city,
+                  country,
+                },
+                {setSubmitting},
+              ) => {
+                await setCustomerAddress({
+                  phoneNumber,
+                  company,
+                  streetLine,
+                  postalCode,
+                  province,
+                  city,
+                  country,
+                })
+                setSubmitting(false)
+              }}
             >
-              <Formik
-                initialValues={{
-                  company: '',
-                  streetLine: '',
-                  phoneNumber: '',
-                  province: '',
-                  postalCode: '',
-                  city: '',
-                  country: '',
-                }}
-                validate={async (values) => {
-                  const schema = Yup.object({
-                    streetLine: Yup.string()
-                      .required('Required'),
-                    phoneNumber: Yup.number()
-                      .positive('Must be a positive number')
-                      .required('Required'),
-                    city: Yup.string()
-                      .required('Required'),
-                    province: Yup.string()
-                      .required('Required'),
-                    postalCode: Yup.string()
-                      .required('Required'),
-                    country: Yup.string()
-                      .required('Required'),
-                  })
+              <Form className={s.fieldset}>
+                <TextInput
+                  label="Company"
+                  name="company"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="Street and House Number"
+                  name="streetLine"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="Phone Number"
+                  name="phoneNumber"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="Province"
+                  name="province"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="Postal Code"
+                  name="postalCode"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="City"
+                  name="city"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <TextInput
+                  label="Country"
+                  name="country"
+                  type="text"
+                  placeholder="Jane"
+                />
 
-                  return await schema.validate(values, { abortEarly: false })
-                  .then(() => {})
-                  .catch((err: Yup.ValidationError) => {
-                    const errors = err.inner
-                    return errors.reduce<{[key: string]: string[]}>((a, error) => {
-                      const field = error.path
-                      const params = error.params
-    
-                      if (!params || !field) {
-                        return a
-                      }
-    
-                      const errorMessages = error.type !== 'typeError'? (
-                        error.errors
-                      ) : params.originalValue !== "" ? (
-                        [`Must be a ${params.type}`]
-                      ) : (
-                        ["Required"]
-                      )
-    
-                      return field in a ? (
-                        {...a, [field]: [...a[field], ...errorMessages]}
-                      ) : (
-                        {...a, [field]: errorMessages}
-                      )
-                    }, {})
-                  })
-                }}
-                onSubmit={async (
-                  {
-                    phoneNumber,
-                    company,
-                    streetLine,
-                    postalCode,
-                    province,
-                    city,
-                    country,
-                  },
-                  {setSubmitting},
-                ) => {
-                  await setCustomerAddress({
-                    phoneNumber,
-                    company,
-                    streetLine,
-                    postalCode,
-                    province,
-                    city,
-                    country,
-                  })
-                  setSubmitting(false)
-                }}
-              >
-                <Form className={s.fieldset}>
-                  <TextInput
-                    label="Company"
-                    name="company"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="Street and House Number"
-                    name="streetLine"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="Phone Number"
-                    name="phoneNumber"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="Province"
-                    name="province"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="Postal Code"
-                    name="postalCode"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="City"
-                    name="city"
-                    type="text"
-                    placeholder="Jane"
-                  />
-                  <TextInput
-                    label="Country"
-                    name="country"
-                    type="text"
-                    placeholder="Jane"
-                  />
-
-                  <Button
-                    type="submit"
-                    width="100%"
-                    variant="ghost"
-                  >
-                    Save
-                  </Button>
-                </Form>
-              </Formik>
-            </Dialog.Content>
-          </Dialog.Overlay>
-        </div>
+                <Button
+                  type="submit"
+                  width="100%"
+                  variant="ghost"
+                >
+                  Save
+                </Button>
+              </Form>
+            </Formik>
+          </Dialog.Content>
+        </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
   )
